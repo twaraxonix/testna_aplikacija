6cb0bd0b6d674b293c68165e6995ab976ef74a2c
Twaraxonix <rok.zerdoner@gmail.com>
2022-10-26 13:29:30 +0200

    ustvarjanje objekta prijavni podatki in vnašanje njegovih lastnosti ter metod

---
+++ testna_aplikacija/PrijavniPodatki.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testna_aplikacija
+{
+    public class PrijavniPodatki
+    {
+        string ime, priimek, eNaslov, geslo;
+
+        PrijavniPodatki(string Ime, string Priimek, string ENaslov, string Geslo) {
+            ime = Ime;
+            priimek = Priimek;
+            eNaslov = ENaslov;
+            geslo = Geslo;
+        }
+
+        string izpisImenainPriimka() {
+            return ime + " " + priimek;
+        }
+    }
+}
